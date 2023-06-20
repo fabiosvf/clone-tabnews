009663d33b274e3d8fcc296c59baa83a93176965
fabiosvf <53756627+fabiosvf@users.noreply.github.com>
2023-06-20 23:34:25 +0000

    Primeiro commit e primeiro desafio

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Regina<br/>É top demais ter alguém como quem eu posso contar e compartilhar projetos para o nosso futuro. Te amo de um jeito que não posso explicar em palavras, mas te garanto que o céu é o limite onde vamos estar.<br/>Fabio</h1>
+}
+
+export default Home;
