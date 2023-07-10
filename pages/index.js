deb884538cc62972e1c2896c3ca790d44f217403
fabiosvf <53756627+fabiosvf@users.noreply.github.com>
2023-07-10 00:53:47 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Regina<br/>É top demais ter alguém como quem eu posso contar e compartilhar projetos para o nosso futuro. Te amo de um jeito que não posso explicar em palavras, mas te garanto que o céu é o limite onde vamos estar.<br/>Fabio</h1>
+  return (
+    <h1>
+      Regina
+      <br />É top demais ter alguém como quem eu posso contar e compartilhar
+      projetos para o nosso futuro. Te amo de um jeito que não posso explicar em
+      palavras, mas te garanto que o céu é o limite onde vamos estar.
+      <br />
+      Fabio
+    </h1>
+  );
 }
 
 export default Home;
