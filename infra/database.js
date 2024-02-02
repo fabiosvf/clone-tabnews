50be02d7b05ee052969c02ce077caede5b506fd6
fabiosvf <53756627+fabiosvf@users.noreply.github.com>
2024-02-02 01:05:16 +0000

    makes   more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,23 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
+  const config = {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-
-  await client.connect();
+  };
+  const client = new Client(config);
+  console.log(config);
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
