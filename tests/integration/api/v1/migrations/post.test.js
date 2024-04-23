88174a0c353b93b9dbbda942cb0a68f543fffa13
fabiosvf <53756627+fabiosvf@users.noreply.github.com>
2024-04-23 11:33:58 +0000

    adds  endpoint

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -32,26 +32,23 @@ async function getNumMigrationsApplied() {
 }
 
 test("POST to /api/v1/migrations should return 200", async () => {
-  // Tabela `pgmigrations` não deve existir antes do processo
-  let hasMigrationTable = await getHasMigrationTable();
-  expect(hasMigrationTable).toBe(false);
-
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
-  expect(response.status).toBe(200);
+  expect(response1.status).toBe(201);
 
-  // Tabela `pgmigrations` deve existir após o processo
-  hasMigrationTable = await getHasMigrationTable();
-  expect(hasMigrationTable).toBe(true);
+  const response1Body = await response1.json();
 
-  const responseBody = await response.json();
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
 
-  // Retorno da API deve ser um array com uma lista de Migrations
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
 
-  // O número de Migrations obtidas da API deve ser igual ao que foi aplicado no Banco de Dados
-  const numMigrationsApplied = await getNumMigrationsApplied();
-  expect(numMigrationsApplied).toBe(responseBody.length);
+  const response2Body = await response2.json();
+
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
 });
