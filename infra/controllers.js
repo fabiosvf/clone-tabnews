d46963c1ea505dfeb85ec64664ece41c7cf40a70
Fabio Silva Ferreira <fabiosvf@gmail.com>
2026-05-02 22:22:14 -0300

    feat: create `authorization` model and use it inside `/sessions` controller

--- infra/controllers.js
+++ infra/controllers.js
@@ -1,6 +1,8 @@
 import * as cookie from "cookie";
 import session from "models/session";
-import { user } from "models/user";
+import user from "models/user";
+import authorization from "models/authorization";
+
 import {
   InternalServerError,
   MethodNotAllowedError,
@@ -72,7 +74,7 @@ async function injectAnonymousOrUser(request, response, next) {
 async function injectAuthenticatedUser(request) {
   const sessionToken = request.cookies.session_id;
   const sessionObject = await session.findOneValidByToken(sessionToken);
-  const userObject = await user.findOneById(sessionObject.userId);
+  const userObject = await user.findOneById(sessionObject.user_id);
 
   request.context = {
     ...request.context,
@@ -95,7 +97,7 @@ function canRequest(feature) {
   return function canRequestMiddleware(request, response, next) {
     const userTryingToRequest = request.context.user;
 
-    if (userTryingToRequest.features.includes(feature)) {
+    if (authorization.can(userTryingToRequest, feature)) {
       return next();
     }
 
--- tests/integration/api/v1/sessions/post.test.js
+++ tests/integration/api/v1/sessions/post.test.js
@@ -2,6 +2,7 @@ import { version as uuidVersion } from "uuid";
 import setCookieParser from "set-cookie-parser";
 import orchestrator from "tests/orchestrator";
 import session from "models/session";
+import user from "models/user";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -98,6 +99,8 @@ describe("POST /api/v1/sessions", () => {
         email: "tudo.correto@gmail.com",
         password: "tudocorreto",
       });
+
+      await orchestrator.activateUser(createdUser);
 
       const response = await fetch("http://localhost:3000/api/v1/sessions", {
         method: "POST",
